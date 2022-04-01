67c46b1315f5af1f0dc4140f4c15a73ca70a3fed
Iago <iagojlourenco@gmail.com>
2022-04-01 18:01:51 -0300

    Todas as instruções sendo parseadas

--- instructions.py
+++ instructions.py
@@ -2,7 +2,7 @@
 
 class RType:
 
-    OPCODE = int(0b00000000000000000000000000110011)
+    OPCODE = [int(0b00000000000000000000000000110011)]
 
     def __init__(self, opcode, rd, rs1, rs2, funct3, funct7) -> None:
         self.opcode = opcode
@@ -41,7 +41,9 @@ class RType:
 
 
 class IType:
-    OPCODE = int(0b00000000000000000000000000010011)
+    OPCODE = [int(0b00000000000000000000000000100111),
+              int(0b00000000000000000000000000000011),
+              int(0b00000000000000000000000000010011)]
 
     def __init__(self, opcode, rd, rs1, funct3, imm) -> None:
         self.opcode = opcode
@@ -76,7 +78,7 @@ class IType:
 
 class SType:
 
-    OPCODE = int(0b00000000000000000000000000100011)
+    OPCODE = [int(0b00000000000000000000000000100011)]
 
     def __init__(self, opcode, funct3, rs1, rs2, imm) -> None:
         self.opcode = opcode
@@ -108,3 +110,97 @@ class SType:
                      (instruction & rs1_mask) >> 15,
                      (instruction & rs2_mask) >> 20,
                      ((instruction & imm11_5_mask) >> 21) + (instruction & imm4_0_mask) >> 7)
+
+
+class BType:
+
+    OPCODE = [int(0b00000000000000000000000001100011)]
+
+    def __init__(self, opcode, rs1, rs2, funct3, imm) -> None:
+        self.opcode = opcode
+        self.rs1 = rs1
+        self.rs2 = rs2
+        self.funct3 = funct3
+        self.imm = imm
+
+    def print(self):
+        # Imprime os dados da classe
+        print("opcode:", bin(self.opcode).replace('0b', '').zfill(7))
+        print("rs1:", bin(self.rs1).replace('0b', '').zfill(5))
+        print("rs2:", bin(self.rs2).replace('0b', '').zfill(5))
+        print("funct3:", bin(self.funct3).replace('0b', '').zfill(3))
+        print("imm:", bin(self.imm).replace('0b', '').zfill(12))
+        print("\n")
+
+    @staticmethod
+    def parse_instruction(instruction):
+        opcode_mask = 0b00000000000000000000000001111111
+        imm11_mask = 0b00000000000000000000000010000000
+        imm4_1_mask = 0b00000000000000000000111100000000
+        funct3_mask = 0b00000000000000000111000000000000
+        rs1_mask = 0b00000000000011111000000000000000
+        rs2_mask = 0b00000001111100000000000000000000
+        imm10_5_mask = 0b01111110000000000000000000000000
+        imm12_mask = 0b10000000000000000000000000000000
+
+        return BType(instruction & opcode_mask,
+                     (instruction & rs1_mask) >> 15,
+                     (instruction & rs2_mask) >> 20,
+                     (instruction & funct3_mask) >> 12,
+                     ((instruction & imm12_mask) >> 19) + ((instruction & imm11_mask) << 4) + ((instruction & imm10_5_mask) >> 20) + ((instruction & imm4_1_mask) >> 7))
+
+
+class UType:
+    OPCODE = [int(0b00000000000000000000000000110111)]
+
+    def __init__(self, opcode, rd, imm) -> None:
+        self.opcode = opcode
+        self.rd = rd
+        self.imm = imm
+
+    def print(self):
+        # Imprime os dados da classe
+        print("opcode:", bin(self.opcode).replace('0b', '').zfill(7))
+        print("rd:", bin(self.rd).replace('0b', '').zfill(5))
+        print("imm:", bin(self.imm).replace('0b', '').zfill(20))
+        print("\n")
+
+    @staticmethod
+    def parse_instruction(instruction):
+        opcode_mask = 0b00000000000000000000000001111111
+        rd_mask = 0b00000000000000000000111110000000
+        imm_mask = 0b11111111111111111111000000000000
+
+        return UType((instruction & opcode_mask),
+                     (instruction & rd_mask) >> 7,
+                     (instruction & imm_mask))
+
+
+class JType:
+
+    OPCODE = [int(0b00000000000000000000000001101111)]
+
+    def __init__(self, opcode, rd, imm) -> None:
+        self.opcode = opcode
+        self.rd = rd
+        self.imm = imm
+
+    def print(self):
+        # Imprime os dados da classe
+        print("opcode:", bin(self.opcode).replace('0b', '').zfill(7))
+        print("rd:", bin(self.rd).replace('0b', '').zfill(5))
+        print("imm:", bin(self.imm).replace('0b', '').zfill(20))
+        print("\n")
+
+    @staticmethod
+    def parse_instruction(instruction):
+        opcode_mask = 0b00000000000000000000000001111111
+        rd_mask = 0b00000000000000000000111110000000
+        imm19_12_mask = 0b00000000000011111111000000000000
+        imm11_mask = 0b00000000000100000000000000000000
+        imm10_1_mask = 0b01111111111000000000000000000000
+        imm20_mask = 0b10000000000000000000000000000000
+
+        return JType((instruction & opcode_mask),
+                     (instruction & rd_mask) >> 7,
+                     ((instruction & imm20_mask) >> 11) + (instruction & imm19_12_mask) + ((instruction & imm11_mask) >> 9) + ((instruction & imm10_1_mask) >> 20))
--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import sys
 
-from instructions import IType, RType, SType
+from instructions import BType, IType, JType, RType, SType, UType
 
 input = sys.argv[1]
 
@@ -22,16 +22,31 @@ while(True):
     opcode = binary_data & 0b00000000000000000000000001111111
     #print("OPCODE", bin(opcode).replace('0b', '').zfill(7))
 
-    if opcode == IType.OPCODE:
+    if opcode in IType.OPCODE:
         print("IType")
         pc = IType.parse_instruction(binary_data)
         pc.print()
-    elif opcode == RType.OPCODE:
+    elif opcode in RType.OPCODE:
         print("RType")
         pc = RType.parse_instruction(binary_data)
         pc.print()
-    elif opcode == SType.OPCODE:
+    elif opcode in SType.OPCODE:
         print("SType")
         pc = SType.parse_instruction(binary_data)
         pc.print()
-        pass
+    elif opcode in BType.OPCODE:
+        print("BType")
+        pc = BType.parse_instruction(binary_data)
+        pc.print()
+    elif opcode in UType.OPCODE:
+        print("UType")
+        pc = UType.parse_instruction(binary_data)
+        pc.print()
+    elif opcode in JType.OPCODE:
+        print("JType")
+        pc = JType.parse_instruction(binary_data)
+        pc.print()
+    else:
+        print("Unknown instruction", bin(
+            binary_data).replace('0b', '').zfill(32))
+        exit(-1)
